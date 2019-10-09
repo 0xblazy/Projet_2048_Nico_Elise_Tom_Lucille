2c4e45458221b3274e7a5193e5cf6856dbdc455d
TomWyso <tomwysocki@icloud.com>
2019-10-09 17:11:15 +0200

    Fonction getVoisinDirect

--- Projet_2048/src/model/Case.java
+++ Projet_2048/src/model/Case.java
@@ -5,6 +5,14 @@
  */
 package model;
 
+import static model.Parametres.ARRIERE;
+import static model.Parametres.AVANT;
+import static model.Parametres.BAS;
+import static model.Parametres.DROITE;
+import static model.Parametres.GAUCHE;
+import static model.Parametres.HAUT;
+import static model.Parametres.TAILLE;
+
 /**
  *
  * @author nKBlaZy
@@ -66,5 +74,47 @@ public class Case {
 
     public void setZ(int z) {
         this.z = z;
-    } 
+    }
+    
+    public Case getVoisinDirect(int direction){
+        switch(direction){
+            case HAUT:
+                if(x == 0){
+                    return null;
+                } else{
+                    return cube.getCube()[z][y][x-1];
+                }
+            case BAS:
+                if(x == TAILLE - 1){
+                    return null;
+                } else{
+                    return cube.getCube()[z][y][x+1];
+                }
+            case DROITE:
+                if(y == TAILLE - 1){
+                    return null;
+                } else{
+                    return cube.getCube()[z][y+1][x];
+                }
+            case GAUCHE:
+                if(y == 0){
+                    return null;
+                } else{
+                    return cube.getCube()[z][y-1][x];
+                }
+            case AVANT:
+                if(y == 0){
+                    return null;
+                } else{
+                    return cube.getCube()[z-1][y][x];
+                }
+            case ARRIERE:
+                if(y == TAILLE - 1){
+                    return null;
+                } else{
+                    return cube.getCube()[z+1][y][x];
+                }
+        }
+        return null;
+    }
 }
