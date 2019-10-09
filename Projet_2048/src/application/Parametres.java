ef1d2819fbde7c33a41076e5c62ac48e7771d05c
TomWyso <tomwysocki@icloud.com>
2019-10-09 15:49:00 +0200

    Ajout des paramètres

---
+++ Projet_2048/src/application/Parametres.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+/**
+ *
+ * @author TomWyso
+ */
+public class Parametres {
+    
+    static final int HAUT = 1;  //coordonnées x
+    static final int BAS = -1;
+    
+    static final int DROITE = 2;  //coordonnées y
+    static final int GAUCHE = -2;
+    
+    static final int AVANT = 3;  //coordonnées z
+    static final int ARRIERE = -3;
+    
+    static final int TAILLE = 3;
+    static final int OBJECTIF = 2048;
+    
+}
