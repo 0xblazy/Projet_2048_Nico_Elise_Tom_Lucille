3c5d8d9fcf1a1ad673d4ec73b87852f0fc72896b
Lixye <48949916+Lixye@users.noreply.github.com>
2019-10-24 13:19:26 +0200

    intrface graphique prémisses 3

--- Projet_2048/src/application/FXMLDocumentController.java
+++ Projet_2048/src/application/FXMLDocumentController.java
@@ -10,7 +10,10 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 /**
  *
@@ -18,6 +21,24 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
+    @FXML
+    private Label score; // value will be injected by the FXMLLoader
+    @FXML
+    private Label move_label; //le mot "Move" dans le jeu
+    @FXML
+    private Label nb_move; //Nombre de mouvements effectués
+    @FXML
+    private Pane container; // panneau recouvrant toute la fenêtre
+    @FXML
+    private Pane move_pane; //Le conteneur de move_label + nb_move
+    @FXML
+    private GridPane grid1; //Grille à gauche dans le jeu
+    @FXML
+    private GridPane grid2; //Grille au milieu
+    @FXML
+    private GridPane grid3; //Grille à droite
+    @FXML
+    private Button start_button; //Bouton "START"
     
     //private Cube grilleModele = new Cube(Partie p);
     
