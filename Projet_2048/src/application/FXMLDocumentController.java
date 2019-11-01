8240b267a2d15e09bcd4da9b0e62e4077020169e
Lixye <48949916+Lixye@users.noreply.github.com>
2019-11-01 17:28:51 +0100

    changements mineurs

--- Projet_2048/src/application/FXMLDocumentController.java
+++ Projet_2048/src/application/FXMLDocumentController.java
@@ -14,6 +14,7 @@ import javafx.geometry.HPos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
@@ -58,18 +59,17 @@ public class FXMLDocumentController implements Initializable {
     private MenuItem pt_giveup;
     @FXML
     private MenuItem pt_save;
-    @FXML
     
+    @FXML
     private MenuItem op_theme; //item du menu "Option"
     @FXML
     private MenuItem op_son;
-    @FXML
     
     //private Cube grilleModele = new Cube(Partie p);
     // variables globales non définies dans la vue (fichier .fxml)
     //Cases du jeu
     private final Pane case_pane = new Pane(); // panneau utilisé pour dessiner une tuile "2"
-    private final Label case_label = new Label("2");
+    private final Label case_label = new Label("2");  
     private int x_1 = 22, x_2 = 267, x_3 = 514, y = 270;
     private int objectifx_1 = 22, objectifx_2 = 267, objectifx_3 = 514, objectify = 270;
 
@@ -82,7 +82,7 @@ public class FXMLDocumentController implements Initializable {
         // utilisation de styles pour la grille et la tuile (voir style_2048.css)
         case_pane.getStyleClass().add("case_pane");
         case_label.getStyleClass().add("case_label");
-        
+        //Définition de la taille d'une case
         grid1.getStyleClass().add("grid");
         grid2.getStyleClass().add("grid");
         grid3.getStyleClass().add("grid");
@@ -106,17 +106,32 @@ public class FXMLDocumentController implements Initializable {
         op_son.getStyleClass().add("bouton");
         op_theme.getStyleClass().add("bouton");
         
+        GridPane.setHalignment(case_pane, HPos.CENTER);
         GridPane.setHalignment(case_label, HPos.CENTER);
         grid1.getChildren().add(case_pane); //ajout de la case à son conteneur: la grille 1
         case_pane.getChildren().add(case_label);
-
         // on place la tuile en précisant les coordonnées (x,y) du coin supérieur gauche d'une des trois grille
         case_pane.setLayoutX(x_1);
         case_pane.setLayoutX(x_2);
         case_pane.setLayoutX(x_3);
         case_pane.setLayoutY(y);
+        //On rend les composants créés via le code visibles
         case_pane.setVisible(true);
         case_label.setVisible(true);
+        
+    }
+    @FXML
+    public void keyPressed(KeyEvent ke) {
+        System.out.println("touche appuyée");
+        String touche = ke.getText();
+        if (touche.compareTo("q") == 0) { // utilisateur appuie sur "q" pour envoyer la tuile vers la gauche
+            if (objectifx_1 > 24) { // possible uniquement si on est pas dans la colonne la plus à gauche
+                objectifx_2 -= (int) 397 / 4; // on définit la position que devra atteindre la tuile en abscisse (modèle). Le thread se chargera de mettre la vue à jour
+                move_label.setText(Integer.toString(Integer.parseInt(move_label.getText()) + 1)); // mise à jour du compteur de mouvement
+            }
+        }else if (touche.compareTo("d") == 0) { // utilisateur appuie sur "d" pour envoyer la tuile vers la droite
+
+        }
     }    
     
 }
--- Projet_2048/src/application/Projet_2048.java
+++ Projet_2048/src/application/Projet_2048.java
@@ -51,7 +51,7 @@ public class Projet_2048 extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        stage.setTitle("2048");
         stage.setScene(scene);
         stage.show();
         
