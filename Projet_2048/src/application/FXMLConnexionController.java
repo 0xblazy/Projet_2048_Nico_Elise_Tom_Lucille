062b1dc8d386ac2bc98bb7d396d3174c5cc030a9
Lixye <48949916+Lixye@users.noreply.github.com>
2019-11-18 16:51:26 +0100

    page de connexion

--- Projet_2048/src/application/FXMLConnexionController.java
+++ Projet_2048/src/application/FXMLConnexionController.java
@@ -5,10 +5,12 @@
  */
 package application;
 
+import bdd.BaseDeDonnees;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
@@ -27,20 +29,34 @@ public class FXMLConnexionController implements Initializable {
     @FXML
     private Pane container;
     @FXML
-    private Label label_inscription;
+    private Pane field_pane;
     @FXML
-    private Label label_connexion;
+    private Label label_mdp;
+    @FXML
+    private Label label_identifiant;
     @FXML
     private TextField txt_connexion;
     @FXML
-    private TextField txt_inscription;
+    private TextField txt_mdp;
+    @FXML
+    private Button connexion_bouton;
     
-    
+    private BaseDeDonnees bdd;
     
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        bdd = BaseDeDonnees.getInstance();
+        /*****SET LES TAILLES*****/
+        container.setPrefSize(800, 600);
+        /*****SET LE CSS*****/
+        container.getStyleClass().add("container");
+        field_pane.getStyleClass().add("field_pane");
+        label_mdp.getStyleClass().add("label_id");
+        label_identifiant.getStyleClass().add("label_id");
+        txt_connexion.getStyleClass().add("txt_connexion");
+        txt_mdp.getStyleClass().add("txt_mdp");
+        connexion_bouton.getStyleClass().add("connexion_bouton");
     }    
     
 }
